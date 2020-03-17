bc1b9b1091d2b8a12ca9c9c822ef2ace86ea697a
Christoph Hochstrasser <christoph@hochstrasser.io>
2020-03-17 08:50:35 +0100

    Add greenlock code

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-const https = require('https')
+require('dotenv').config()
+
 const httpProxy = require('http-proxy')
 const fs = require('fs')
 const path = require('path')
 
-const proxy = httpProxy.createProxyServer({
-    target: 'https://sybos.ooelfv.at/',
-    ssl: {
-        cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
-        key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
-    },
-    secure: false,
-    xfwd: false,
-    changeOrigin: true,
-})
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
-proxy.listen(10000)
+;(async () => {
+    let ssl = {}
+
+    if (isDevelopment) {
+        ssl = {
+            cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
+            key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
+        }
+    } else {
+        let pkg = require('./package.json');
+        let Greenlock = require('greenlock');
+
+        let greenlock = Greenlock.create({
+            configDir: './greenlock.d/config.json',
+            packageAgent: pkg.name + '/' + pkg.version,
+            maintainerEmail: pkg.author,
+            staging: true,
+            notify: function(event, details) {
+                if ('error' === event) {
+                    // `details` is an error object in this case
+                    console.error(details);
+                }
+            }
+        });
+
+        const fullConfig = await greenlock.manager.defaults({
+            agreeToTerms: true,
+            subscriberEmail: 'webhosting@example.com'
+        })
+
+        var altnames = [process.env.HOST];
+
+        await greenlock.add({
+            subject: altnames[0],
+            altnames: altnames
+        })
+
+        const pems = await greenlock.get({ servername: process.env.HOST })
+
+        ssl = {
+            cert: pems.cert,
+            key: pems.privkey,
+        }
+    }
+
+    const proxy = httpProxy.createProxyServer({
+        target: 'https://sybos.ooelfv.at/',
+        ssl,
+        secure: false,
+        xfwd: false,
+        changeOrigin: true,
+    })
+
+    proxy.listen(process.env.PORT)
+})()
