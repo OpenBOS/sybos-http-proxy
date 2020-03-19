19329b0486fb56daa4eb7832c8558595034985b3
Christoph Hochstrasser <christoph@hochstrasser.io>
2020-03-19 10:44:59 +0100

    Use certs from env

--- index.js
+++ index.js
@@ -3,57 +3,15 @@ require('dotenv').config()
 const httpProxy = require('http-proxy')
 const fs = require('fs')
 const path = require('path')
-
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 ;(async () => {
-    let ssl = {}
-
-    if (isDevelopment) {
-        ssl = {
-            cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
-            key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
-        }
-    } else {
-        let pkg = require('./package.json');
-        let Greenlock = require('greenlock');
-
-        let greenlock = Greenlock.create({
-            configDir: './greenlock.d/config.json',
-            packageAgent: pkg.name + '/' + pkg.version,
-            maintainerEmail: pkg.author,
-            staging: true,
-            notify: function(event, details) {
-                if ('error' === event) {
-                    // `details` is an error object in this case
-                    console.error(details);
-                }
-            }
-        });
-
-        const fullConfig = await greenlock.manager.defaults({
-            agreeToTerms: true,
-            subscriberEmail: 'webhosting@example.com'
-        })
-
-        var altnames = [process.env.HOST];
-
-        await greenlock.add({
-            subject: altnames[0],
-            altnames: altnames
-        })
-
-        const pems = await greenlock.get({ servername: process.env.HOST })
-
-        ssl = {
-            cert: pems.cert,
-            key: pems.privkey,
-        }
-    }
-
     const proxy = httpProxy.createProxyServer({
         target: 'https://sybos.ooelfv.at/',
-        ssl,
+        ssl: {
+            cert: fs.readFileSync(path.resolve(process.env.SSL_CERT)),
+            key: fs.readFileSync(path.resolve(process.env.SSL_KEY)),
+        },
         secure: false,
         xfwd: false,
         changeOrigin: true,
